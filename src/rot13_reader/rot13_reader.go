0738da0085f5651c17ba8fbbf4d5d93e708ec1b8
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-02 10:33:33 -0400

    add rot13 exercise

---
+++ src/rot13_reader/rot13_reader.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(b []byte) (int, error) {
+	lc_lb := byte('a')
+	lc_ub := byte('z')
+	uc_lb := byte('A')
+	uc_ub := byte('Z')
+	n, err := rot.r.Read(b)
+	for i := 0; i < n; i++ {
+		if b[i] >= lc_lb && b[i] <= lc_ub {
+			if (b[i] - 13) < lc_lb {
+				b[i] = (b[i] - 13 - lc_lb) + lc_ub + 1
+			} else {
+				b[i] = b[i] - 13
+			}
+		} else if b[i] >= uc_lb && b[i] <= uc_ub {
+			if b[i]-13 < uc_lb {
+				b[i] = (b[i] - 13 - uc_lb) + uc_ub + 1
+			} else {
+				b[i] = b[i] - 13
+			}
+		}
+	}
+	if err == io.EOF {
+		return n, err
+	}
+	return n, nil
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
